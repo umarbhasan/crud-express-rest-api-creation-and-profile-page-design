58100405d08924d0b27de7dd89cbf57f172aff2b
Umar Hasan <umar.b.hasan@outlook.com>
2024-05-25 23:44:12 +0600

    Commented assignment2.sql

--- .db/assignment2.sql
+++ .db/assignment2.sql
@@ -1,13 +1,16 @@
+-- Create a new database named 'assignment2'
 CREATE DATABASE assignment2;
 
-use assignment2;
+-- Use the newly created database
+USE assignment2;
 
+-- Create a table named 'users' to store user information
 CREATE TABLE users (
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    gender ENUM('Male', 'Female') NOT NULL,
-    date_of_birth DATE NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    profile_image_url VARCHAR(255)
+    first_name VARCHAR(255) NOT NULL,            -- First name of the user, cannot be null
+    last_name VARCHAR(255) NOT NULL,             -- Last name of the user, cannot be null
+    gender ENUM('Male', 'Female') NOT NULL,      -- Gender of the user, must be either 'Male' or 'Female'
+    date_of_birth DATE NOT NULL,                 -- Date of birth of the user, cannot be null
+    email VARCHAR(255) NOT NULL UNIQUE,          -- Email address of the user, must be unique and cannot be null
+    password VARCHAR(255) NOT NULL,              -- Password for user account, cannot be null
+    profile_image_url VARCHAR(255)               -- URL to the profile image of the user, can be null
 );
